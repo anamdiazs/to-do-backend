3430608c2dca0808a604f139eb1b355d57b3b4ff
Ana María Díaz <anamdiazs1212@gmail.com>
2022-09-09 21:16:49 -0500

    config: Set Cors config

--- app/src/main/java/com/todo/app/ToDoAppApplication.java
+++ app/src/main/java/com/todo/app/ToDoAppApplication.java
@@ -2,6 +2,10 @@ package com.todo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class ToDoAppApplication {
@@ -9,5 +13,13 @@ public class ToDoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
